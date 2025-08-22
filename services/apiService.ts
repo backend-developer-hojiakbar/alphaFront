4279df4ec80477018bb6f436f68029e910b61fc5
backend-developer-hojiakbar <soyibnazarovhoji@gmail.com>
2025-08-22 14:20:56 +0500

    first commits

--- services/apiService.ts
+++ services/apiService.ts
@@ -1,6 +1,6 @@
 // src/services/apiService.ts
 
-const API_BASE_URL = 'http://127.0.0.1:8000/api';
+const API_BASE_URL = 'http://printapi.cdcgroup.uz/api';
 
 const getAuthToken = () => {
     return localStorage.getItem('accessToken');
